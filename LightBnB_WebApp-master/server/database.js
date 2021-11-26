65d59b77fd25ffc57b1253163e128546869ccfa4
Amir <amir.a.23@hotmail.com>
2021-11-26 05:06:07 +0000

    Update addProperty function to insert data into  lightbnb database, properties table

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -182,6 +182,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-	
+	const queryArr = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces,
+    property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+  const queryString = `
+    INSERT INTO properties 
+      (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,
+      number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+
+  return pool
+        .query(queryString, queryArr)
+        .then(res => res.rows)
+        .catch(err => console.error(err.message));
 };
 exports.addProperty = addProperty;
